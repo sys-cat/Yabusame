20c0198a9decf58e308e941f83da22a456d5a5fb
sys-cat <systemcat91@gmail.com>
2016-05-04 20:31:58 +0900

    add config

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package Config
+
+import (
+  "github.com/BurntSushi/toml"
+)
+
+type Base struct {
+  Serv Server
+  Auth Auth
+}
+
+type Server struct {
+  PORT string `toml:"PORT"`
+  ENV string `toml:"ENV"`
+}
+
+type Auth struct {
+  SEC string `toml:"SECRET"`
+  SALT string `toml:"SALT"`
+}
+
+func Get()(conf Base) {
+  _, err := toml.DecodeFile("./base.toml", &conf)
+  if err != nil { panic(err) }
+  return conf
+}
